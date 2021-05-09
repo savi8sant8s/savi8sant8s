96ba8a4d8059bbb85be6947339309dee71d2cde0
Savio Santos <saviosa08@gmail.com>
2021-05-09 10:27:22 -0300

    Melhorado Layout e inserido conteudo inicial da pagina.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,43 @@
 import { Component } from "react";
-import { Button, Accordion, Card, Nav, Image, Container, Row, Col } from "react-bootstrap";
+import { 
+  ListGroup, 
+  Button,
+  Accordion, 
+  Card, 
+  Nav, 
+  Image, 
+  Container, 
+  Row, 
+  Col,
+  Badge
+} from "react-bootstrap";
 import TextAnimation from 'react-animate-text';
-
+import { Github, Linkedin} from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const styles = {
+  background: "linear-gradient(#141e30, #243b55)",
+  height: '100vh',
+  minHeight : '100vh',
+  textAlign: "center"
+};
 
 class Menu extends Component {
   render(){
     return (
       <Nav defaultActiveKey="/home" as="ul" className="justify-content-end">
         <Nav.Item as="li">
-          <Nav.Link href="/home">Github</Nav.Link>
+          <Nav.Link href="https://github.com/savi8sant8s" target="_blank"><Github color="#FF7F50" /></Nav.Link>
         </Nav.Item>
         <Nav.Item as="li">
-          <Nav.Link >Dev.to</Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/savi8sant8s" target="_blank"><Linkedin color="#FF7F50" /></Nav.Link>
         </Nav.Item>
         <Nav.Item as="li">
-          <Nav.Link >Linkedin</Nav.Link>
+          <Nav.Link href="https://dev.to/savi8sant8s" target="_blank"><p style={{ color: "#FF7F50" }}>Dev.to</p></Nav.Link>
         </Nav.Item>
       </Nav>
     )
-  } 
+  }
 }
 
 class Title extends Component {
@@ -27,149 +45,195 @@ class Title extends Component {
     return (
       <div className="d-flex justify-content-center">
         <TextAnimation charInterval={this.props.interval}>
-          <p className={this.props.class}>{this.props.value}</p>
+          <p style={{ color: "#FF7F50" }} className={this.props.class}>{this.props.value}</p>
         </TextAnimation>
       </div>
     )
   }
 }
 
-class Text extends Component {
+class AboutMe extends Component {
   render() {
     return (
-      <TextAnimation charInterval={this.props.interval}>
-        <p>{this.props.value}</p>
-      </TextAnimation>
+      <div>
+        <Container>
+          <Row>
+            <Col xs={6}>
+              <Title value="Sobre mim:" class="h6" interval="50" />
+              <Card bg="secondary" border="warning">
+                <Card.Body style={{ textAlign: "justify", color: "white" }}>
+                        <p>Sou um passageiro que visitou os games de PS2,
+                        Super Nintendo e jogos JAR no celular de botão e descobriu
+                        que a mágica que dava vida a eles é uma interessante empreitada.</p>
+                        <p>Utilizador Linux por necessidade e depois por curiosidade, gosto e
+                        fico vislumbrado em ver e fazer códigos simples e com poucos
+                        recursos, mas com grandes poderes, tentando ter como motivação
+                        a engenharia feita em Super Mario e outros jogos antigos que
+                        faziam milagres em kbytes de armazenamento.</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={6}>
+              <Title value="Habilidades:" class="h6" interval="50" />
+              <Card bg="secondary" border="warning">
+                <Card.Body style={{ textAlign: "justify", color: "white" }}>
+                <p><strong>Graduação: </strong>Técnico em Desenvolvimento de Sistemas e estudante de Bacharelado em Eng. de Software.</p>
+                  <p><strong>Tecnologias principais que domino: </strong>Ionic/Angular, Python, SQL, React e Express.</p>
+                  <p><strong>Tecnologias que já tive contato: </strong>C, Java, Rust e Haskell.</p>
+                  <p><strong>Habilidades interperssoais: </strong>autodidata, trabalho colaborativo, pontualidade e atitude.</p>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   }
 }
 
-class AboutMe extends Component {
+class Projects extends Component {
   render(){
     return (
-      <Container>
-        <Row>
-          <Col className="d-flex justify-content-center">
-            <Image width="200px" src="https://avatars.githubusercontent.com/u/50780673?v=4" roundedCircle />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={6}>
-            <Title value="Sobre mim" class="h6" interval="50" />
-            <Card>
-              <Card.Body>
-              <Text value="cskjfjkshfkghkgssggm" interval="50" />
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={6}>
-            <Title value="Skills" class="h6" interval="50" />
-            <Card>
-              <Card.Body>
-              <Text value="cskjfjkshfkghkgssggm" interval="50" />
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div className="p-3">
+        <h1>
+          <TextAnimation charInterval={100}>
+            <Title value="Projetos" class="h3" interval="100" />
+          </TextAnimation>
+        </h1>
+        <Container>
+          <Row>
+            <Col xs={6}>
+              <h3>
+                <TextAnimation charInterval={50}>
+                  <Title value="Rebase Team:" class="h6" interval="100" />
+                </TextAnimation>
+              </h3>
+              <Accordion style={{ textAlign: "justify", color: 'white' }}>
+                <Card bg="secondary" border="warning">
+                  <Card.Header>
+                    <Accordion.Toggle style={{ color: 'yellow' }} as={Button} variant="link" eventKey="0">
+                      <Container>
+                        <Row>
+                          <Col xs={2}>
+                            <Image width="50px" src="https://play-lh.googleusercontent.com/OHuM2_zXDooZgnPokyvt_odffMKBGGmjTs25emLvbCGMupPdBsCZ9oOjAwahf-oVRYo=s180-rw" roundedCircle />
+                          </Col>
+                          <Col>Zona Azul Garanhuns Informativo</Col>
+                        </Row>
+                      </Container>
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body style={{textAlign:"justify"}}>
+                    <p>Disponível na Play Store: <a href="https://play.google.com/store/apps/details?id=com.amstt.zagi" style={{color: "yellow"}} target="_blank"> clique aqui.</a></p>
+                    Esse aplicativo é um projeto da Autarquia Municipal de Segurança, 
+                    Trânsito e Transporte (AMSTT), visando modernizar o sistema vigente 
+                    do estacionamento rotativo. Nessa primeira fase você poderá ter o 
+                    conhecimento sobre várias informações relacionadas a Zona Azul/Marrom, 
+                    tais como: preços, pontos de estacionamento, horários de funcionamentos 
+                    e mu⁫ito mais.
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card bg="secondary" border="warning">
+                  <Card.Header>
+                    <Accordion.Toggle style={{ color: 'yellow' }} as={Button} variant="link" eventKey="1">
+                      <Container>
+                        <Row>
+                          <Col xs={2}>
+                            <Image width="50px" src="https://play-lh.googleusercontent.com/aqYpQIpe0HPyOkB9t-JplmOcT4ZrIoZOihQi5TWMLpCZASQuAQ3ol58NtZri3LesSlM=s180-rw" roundedCircle />
+                          </Col>
+                          <Col>Aplicativo UPE Plus</Col>
+                        </Row>
+                      </Container>
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body style={{textAlign:"justify"}}>
+                    <p>Disponível na Play Store: <a href="https://play.google.com/store/apps/details?id=com.rebase.alunoupe" style={{color: "yellow"}} target="_blank"> clique aqui.</a></p>
+                    Aplicativo voltado para os estudantes, professores, gestores e demais 
+                    integrantes da Universidade visando controlar o fluxo de pessoas nas 
+                    dependências dos campus - Garanhuns, Salgueiro, Arcoverde e Serra Talhada.
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card bg="secondary" border="warning">
+                  <Card.Header>
+                    <Accordion.Toggle style={{ color: 'yellow' }} as={Button} variant="link" eventKey="2">
+                      <Container>
+                        <Row>
+                          <Col xs={2}>
+                            <Image style={{ background: "white" }} width="50px" src="https://camo.githubusercontent.com/260de492f3382fdaa0ce3d8b23eb7871e5b8b15c9b998a7ea7be42a7425589da/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3731333339313737373737313638333939302f3731393630363435303832303837343234302f49434f4e5f636f7069612e706e673f77696474683d343032266865696768743d343032" roundedCircle />
+                          </Col>
+                          <Col>Garanhuns X Covid-19</Col>
+                        </Row>
+                      </Container>
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="2">
+                    <Card.Body style={{textAlign:"justify"}}>
+                    <p>Código-fonte: <a href="https://github.com/Rebase-team/Guns-x-Covid" style={{color: "yellow"}} target="_blank"> clique aqui.</a></p>
+                    O aplicativo visa engajar a sociedade a contribuir com 
+                    o bem-estar da cidade de Garanhuns, nesse tempo de 
+                    pandemia do coronavírus.
+                    <p>Notícia no G1: <a href="https://g1.globo.com/pe/caruaru-regiao/noticia/2020/06/23/estudantes-da-upe-garanhuns-desenvolvem-aplicativo-para-informar-sobre-aglomeracoes.ghtml" style={{color: "yellow"}} target="_blank"> clique aqui.</a></p>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Col>
+            <Col xs={6}>
+              <h3>
+                <TextAnimation charInterval={50}>
+                <Title value="Pessoal" class="h6" interval="100" />
+                </TextAnimation>
+              </h3>
+              <Accordion>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      Gerador de Competições de Futebol
+          </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>Hello! I'm the body</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                      Crie um Card Game
+          </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>Hello! I'm another body</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                      Mais...
+          </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>Hello! I'm another body</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   }
-}
-
-function Projects() {
-  return (
-    <div className="p-3">
-      <h1 className="d-flex justify-content-center">
-        <TextAnimation charInterval={100}>Projetos</TextAnimation>
-      </h1>
-      <Container>
-        <Row>
-          <Col xs={6} className="justify-content-center">
-            <h3 className="d-flex justify-content-center">
-              <TextAnimation charInterval={50}>Rebase Team</TextAnimation>
-            </h3>
-            <Accordion>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    Zona Azul Garanhuns Informativo
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    UPE Plus
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    Garanhuns X Covid-19
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          </Col>
-          <Col xs={6} className="justify-content-center">
-            <h3 className="d-flex justify-content-center">
-              <TextAnimation charInterval={50}>Pessoal</TextAnimation>
-            </h3>
-            <Accordion>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    Gerador de Competições de Futebol
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    Crie um Card Game
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    Mais...
-        </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  )
 }
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div style={styles}>
         <Menu />
         <Title value="Sávio Santos" class="h1" interval="100" />
+        <Image width="200px" src="https://avatars.githubusercontent.com/u/50780673?v=4" roundedCircle />
         <AboutMe />
         <Projects />
       </div>
